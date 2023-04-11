4b1b225f0aaa0b0ee649554350f788e525d84625
Kevin <50956030+nflhm@users.noreply.github.com>
2023-04-11 15:47:34 +0700

    init

---
+++ src/main/java/id/co/fun/travel/customer/CustomerApplication.java
@@ -0,0 +1,13 @@
+package id.co.fun.travel.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/fun/travel/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.fun.travel.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
