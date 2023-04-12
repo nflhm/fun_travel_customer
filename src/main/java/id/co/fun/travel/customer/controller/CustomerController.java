74c8ee1e2ed9d09bb55ed64c7bcf95fe3166d82b
Kevin <gunawankevin54@gmail.com>
2023-04-12 08:39:39 +0700

    dev customer repository, model, & controller.

---
+++ src/main/java/id/co/fun/travel/customer/controller/CustomerController.java
@@ -0,0 +1,50 @@
+package id.co.fun.travel.customer.controller;
+
+import id.co.fun.travel.customer.model.Customer;
+import id.co.fun.travel.customer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("customer")
+public class CustomerController {
+    @Autowired
+    CustomerRepository customerRepository;
+    @GetMapping("/")
+    public @ResponseBody List<Customer> selectAll() { return customerRepository.selectAll(); }
+    @GetMapping("/find")
+    public @ResponseBody Customer selectUnique(@RequestParam("id") int id) {
+        return customerRepository.selectUnique(id);
+    }
+    @GetMapping("/add")
+    public String addCustomer(@RequestParam("username") String username,
+                              @RequestParam("name") String name,
+                              @RequestParam("password") String password,
+                              @RequestParam("email") String email) {
+        Customer customer = new Customer(0, username, name, password, email, 0);
+        customerRepository.add(customer);
+        return "redirect:/customer/";
+    }
+    @GetMapping("/update")
+    public String updateCustomer(@RequestParam("id") int id,
+                                 @RequestParam("username") String username,
+                                 @RequestParam("name") String name,
+                                 @RequestParam("password") String password,
+                                 @RequestParam("email") String email) {
+        Customer modcustomer = new Customer(id, username, name, password, email, 0);
+        customerRepository.update(modcustomer);
+        return "redirect:/customer/";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("id") int id) {
+        customerRepository.delete(id);
+        return "redirect:/customer/";
+    }
+}
---
+++ src/main/java/id/co/fun/travel/customer/model/Customer.java
@@ -0,0 +1,69 @@
+package id.co.fun.travel.customer.model;
+
+public class Customer {
+    private int customerId;
+    private String username;
+    private String name;
+    private String password;
+    private String email;
+    private int deleteFlag;
+    public Customer() {
+    }
+
+    public Customer(int customerId, String username, String name, String password, String email, int deleteFlag) {
+        this.customerId = customerId;
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.deleteFlag = deleteFlag;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(int deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+}
